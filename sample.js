4e407a4b0c6c04ef15f0ce008eacec03c0c11576
TigRig <tigrig.crex@gmail.com>
2026-05-27 10:57:11 +0900

    脆弱性コミット2

--- sample.js
+++ sample.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
   res.send(`<html><body><h1>User comment: ${userComment}</h1></body></html>`);
 });
 
+app.get('/zeijaku', (req, res) => {
+  const userComment = req.query.comment || '';
+  res.send(`<html><body><h1>User comment: ${userComment}</h1></body></html>`);
+});
+
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
