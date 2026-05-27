6200d9b9668c0e3f5d7652d226d15e395f8cc1b8
TigRig <tigrig.crex@gmail.com>
2026-05-27 11:03:14 +0900

    脆弱性コミット2

--- sample.js
+++ sample.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  const userComment = req.query.comment || '';
-  res.send(`<html><body><h1>User comment: ${userComment}</h1></body></html>`);
-});
-
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
+});
+
+app.get('/hoge', (req, res) => {
+  res.send(`<html><body><h1>User comment: ${req.query.comment || ''}</h1></body></html>`);
 });
