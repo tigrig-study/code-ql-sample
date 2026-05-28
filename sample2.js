6b45a75ac50c292db08ab79381b28d42dd8e18e1
TigRig <tigrig.crex@gmail.com>
2026-05-28 17:54:31 +0900

    脆弱性追加

--- sample2.js
+++ sample2.js
@@ -4,7 +4,10 @@ const username = urlParams.get('name');
 // 脆弱性：ユーザー入力をそのままHTMLとして出力している
 document.getElementById('welcome-message').innerHTML = "Hello, " + username;
 
-app.get('/', (req, res) => {
+app.get('/fuga', (req, res) => {
   const userComment = req.query.comment || '';
   res.send(`<html><body><h1>User comment: ${userComment}</h1></body></html>`);
 });
+
+// 脆弱性：ユーザー入力をそのままHTMLとして出力している
+document.getElementById('welcome-message').innerHTML = "Hello, " + username;
