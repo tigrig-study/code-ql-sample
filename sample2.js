87cb94cf330258be74656c97aa12c1ed7b4b2794
TigRig <tigrig.crex@gmail.com>
2026-05-28 17:12:17 +0900

    Refactor user input handling in sample2.js
    Refactor code to improve security by avoiding direct HTML output of user input.

--- sample2.js
+++ sample2.js
@@ -3,3 +3,7 @@ const username = urlParams.get('name');
 
 // 脆弱性：ユーザー入力をそのままHTMLとして出力している
 document.getElementById('welcome-message').innerHTML = "Hello, " + username;
+
+app.get('/fuga', (req, res) => {
+  res.send(`<html><body><h1>User comment: ${req.query.comment || ''}</h1></body></html>`);
+});
